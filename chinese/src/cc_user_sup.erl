7607153b1ef31e4a531f09657038e7bcd2ec795e
Emil Falk <emil.falk.1988@gmail.com>
2012-04-19 21:08:05 +0200

    Fixed so that the server reuses address

--- chinese/src/cc_user_sup.erl
+++ chinese/src/cc_user_sup.erl
@@ -40,7 +40,7 @@ start_link() ->
 %%--------------------------------------------------------------------
 init([]) ->
     {ok, Port} = application:get_env(port),
-    {ok, ListenSocket} = gen_tcp:listen(Port, [{active, true}, {packet, line}]),
+    {ok, ListenSocket} = gen_tcp:listen(Port, [{reuseaddr, true}, {active, true}, {packet, line}]),
     spawn_link(fun empty_listeners/0),
     {ok, {{simple_one_for_one, 60, 3600},
           [{users,
