7ef0010fe5510b43ff6d73899a5f2421cc5fa839
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-05-11 12:50:55 +0200

    winners

--- arena/evaluate.py
+++ arena/evaluate.py
@@ -29,6 +29,7 @@ def load_stats(filename):
     return res
 
 def player_plies(stats):
+    '''Average of plies depending on the number of players'''
     plies=[None,None,[],[],[],[],[]]
     print "Plies in players"
     for i in stats:
@@ -40,8 +41,38 @@ def player_plies(stats):
         if len(plies[i]) == 0: continue
         print i, reduce((lambda x,y:x+y),plies[i],0) / float(len(plies[i]))
 
+def winners(stats):
+    winners=[None,None,[],[],[],[],[]]
+    players=[None,None,[],[],[],[],[]]
+    stuck=[]
+    print "Winner stats"
+    for i in stats:
+        if 'winner' in i:
+            winners[len(i['players'])].append(i['players'][i['winner'][0]])
+            players[len(i['players'])] += [i['players'][l_] for l_ in i['players']]
+        else:
+            stuck.append(i)
+    for i in xrange(2,len(winners)):
+        d={}
+        for w in winners[i]:
+            if w not in d:
+                d[w]=1
+            else:
+                d[w]+=1
+        r=[]
+        for w in d:
+            r.append((w,d[w]))
+        r.sort(key=(lambda x: x[1]*-1))
+        
+        print "%d" % i
+        for l in r:
+            print "\t\t%s: %d%%\t" %(l[0],float(l[1])*100/float(players[i].count(l[0])))
+    print players        
+    return stuck
+            
 def main():        
-    stats=load_stats("stats.cpython")
+    stats=load_stats("stats.txt")
     player_plies(stats)
+    stuck=winners(stats)
 
 main()    
